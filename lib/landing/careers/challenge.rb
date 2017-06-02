a732952f05e59a80f2201c60a39c24e5726c0c99
Marcelo Jacobus <marcelo.jacobus@xing.com>
2017-06-02 14:33:12 +0100

    Initial commit

---
+++ lib/landing/careers/challenge.rb
@@ -0,0 +1,9 @@
+require "landing/careers/challenge/version"
+
+module Landing
+  module Careers
+    module Challenge
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/landing/careers/challenge/version.rb
@@ -0,0 +1,7 @@
+module Landing
+  module Careers
+    module Challenge
+      VERSION = "0.1.0"
+    end
+  end
+end
